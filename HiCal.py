9d2d1e5e2c1b6d18e734cd6ac5c9102ca8dd59e6
chibitofu <erinmoony@gmail.com>
2020-01-15 11:50:46 -0800

    Initial commit.

---
+++ HiCal.py
@@ -0,0 +1,14 @@
+## Know when a team is having a meeting.
+## 30min blocks past 9am.
+## Stored in tuples (start_time, end_time).
+## ex (2, 3) meeting from 10:00am - 10:30am.
+## Write a function merge_ranges() that takes a list of multiple meeting time ranges and returns a list of condensed ranges.
+
+meeting_times = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+
+
+def merge_ranges(input):
+
+
+
+merge_ranges(meeting_times)
