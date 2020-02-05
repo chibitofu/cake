7099cf7f39046137167a8caa3ff263bdff42e88a
chibitofu <erinmoony@gmail.com>
2020-02-04 16:23:20 -0800

    Refactored

--- match-paren.js
+++ match-paren.js
@@ -29,26 +29,58 @@ Input: "{[]}"
 Output: true
 */
 
-var isValid = function(s) {
-    const charArr = s.split('');
-    const charDict = {")" : "(", "}" : "{", "]" : "["};
-    let checkArr = [];
-    for (let i = 0; i < charArr.length; i++) {
-        const par = charArr[i];
-        if (charDict[par]) {
-            const idx = checkArr.lastIndexOf(charDict[par])
-            if (idx == -1) {
-                return false
-            } else {
-                if (idx !== checkArr.length - 1) {
-                    return false
-                } else {
-                    checkArr.splice(idx, 1);
-                }
-            }
+const test1 = "[()]{}{[()()]()}";
+const test2 = "[(])";
+const test3 = "[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]";
+const test4 = "";
+const test5 = ")";
+const test6 = "()";
+
+const testCases = [test1, test2, test3, test4, test5, test6];
+
+// this solution is O(n^2)
+// var isValid = function(s) {
+//     const charArr = s.split('');
+//     const charDict = {")" : "(", "}" : "{", "]" : "["};
+//     let checkArr = [];
+//     for (let i = 0; i < charArr.length; i++) {
+//         const par = charArr[i];
+//         if (charDict[par]) {
+//             const idx = checkArr.lastIndexOf(charDict[par])
+//             if (idx == -1) {
+//                 return false
+//             } else {
+//                 if (idx !== checkArr.length - 1) {
+//                     return false
+//                 } else {
+//                     checkArr.splice(idx, 1);
+//                 }
+//             }
+//         } else {
+//             checkArr.push(par)
+//         }
+//     }
+//     return checkArr.length == 0 ? true : false
+// };
+
+let isValid = (input) => {
+    const parArr = input.split('');
+    const parDict = {'(': ')', '[': ']', '{': '}'};
+    const stack = [];
+
+    for (let i = 0; i < parArr.length; i++) {
+        const par = parArr[i];
+
+        if (par === '(' || par === '[' || par === "{") {
+            stack.push(par)
         } else {
-            checkArr.push(par)
+            const closePar = stack.pop();
+            if (parDict[closePar] !== par) {
+                return false
+            }
         }
     }
-    return checkArr.length == 0 ? true : false
-};
+    return stack.length === 0 ? true : false
+}
+
+testCases.map(item => console.log(isValid(item)))
